5fac6ac1a4a8481dbf77638d3b47c8ae197121f6
Sara Herrera Ramirez <saraherreraramirez@Rosita.local>
2024-09-21 10:08:32 -0500

    test login

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,2 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
+    public void loginExitoso(){
+        System.setProperty("webdriver.chrome.driver","C://tools//chromedriver");
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+
+    }
 }
