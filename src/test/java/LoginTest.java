0cfba2ded92917eef7bdb46dd4b2b425e937a967
Sara Herrera Ramirez <saraherreraramirez@Rosita.local>
2024-09-21 10:35:30 -0500

    Test with manual chromedriver

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,11 +1,16 @@
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
+    @Test
     public void loginExitoso(){
-        System.setProperty("webdriver.chrome.driver","C://tools//chromedriver");
+        System.setProperty("webdriver.chrome.driver","./chromedriver");
         WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
+
+        driver.get("https://saucedemo.com/");
+        driver.quit();
 
     }
 }
