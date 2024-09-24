41f7491db18f8e7acae5436edaa5f836c3c03cd7
Sara Herrera Ramirez <saraherreraramirez@192-168-1-12.oclc.org>
2024-09-24 16:08:46 -0500

    Base inicial realizada en clase

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,16 +1,48 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LoginTest {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.close();
+    }
+
     @Test
     public void loginExitoso(){
-        System.setProperty("webdriver.chrome.driver","./chromedriver");
-        WebDriver driver= new ChromeDriver();
-        driver.manage().window().maximize();
-
-        driver.get("https://saucedemo.com/");
-        driver.quit();
+        //ARRANGE
+        driver.get("https://www.saucedemo.com/");
+        //ACT
+        WebElement inputUser = driver.findElement(By.id("user-name"));
+        WebElement inputPassword = driver.findElement(By.id("password"));
+        WebElement buttonLogin = driver.findElement(By.id("login-button"));
+        inputUser.sendKeys("standard_user");
+        inputPassword.sendKeys("secret_sauce");
+        buttonLogin.click();
+        //ASSERT
+        WebElement pageTitle = driver.findElement(By.xpath("//span[@data-test='title']"));
+        assertEquals("Products", pageTitle.getText());
 
     }
 }
